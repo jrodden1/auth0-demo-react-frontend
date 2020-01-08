e9e4605aab88254d37696968286567c6c87f73e3
Jeremiah Rodden <jrodden1@gmail.com>
2020-01-08 16:12:44 -0700

    added react-router-dom package

---
+++ src/utils/history.js
@@ -0,0 +1,4 @@
+//This file is responsible for exporting react-router's history module so that auth0 can redirect the user programmatically
+import { createBrowserHistory } from "history";
+
+export default createBrowserHistory();
